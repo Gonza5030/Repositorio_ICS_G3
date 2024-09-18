bd83cb8355b10b6fd203119f8eac8ed016615e0f
unknown <joaquin_spsoetti88@gmail.com>
2024-09-18 10:28:54 -0300

    todas las pruebas de usuario ready

--- TrabajosPracticos/TrabajosPracticosEvaluables/TP_6/TP 6/main.py
+++ TrabajosPracticos/TrabajosPracticosEvaluables/TP_6/TP 6/main.py
@@ -40,53 +40,59 @@ def procesar_pago_view():
     forma_pago = request.form.get("forma_pago")
 
     if forma_pago == "tarjeta":
-        numero_tarjeta = request.form.get("numero_tarjeta")
-        pin = request.form.get("pin")
-        nombre_completo = request.form.get("nombre_completo")
-        tipo_documento = request.form.get("tipo_documento")
-        numero_documento = request.form.get("numero_documento")
-        fecha_vto = request.form.get("fecha_vencimiento")
+        if cotizacion.estado != "Confirmado":
+            numero_tarjeta = request.form.get("numero_tarjeta")
+            pin = request.form.get("pin")
+            nombre_completo = request.form.get("nombre_completo")
+            tipo_documento = request.form.get("tipo_documento")
+            numero_documento = request.form.get("numero_documento")
+            fecha_vto = request.form.get("fecha_vencimiento")
 
-        es_valido, mensaje = validar_tarjeta(
-            numero_tarjeta,
-            pin,
-            nombre_completo,
-            tipo_documento,
-            numero_documento,
-            fecha_vto,
-        )
-        if not es_valido:
-            return mensaje  # Devuelve el mensaje de error de validación
+            es_valido, mensaje = validar_tarjeta(
+                numero_tarjeta,
+                pin,
+                nombre_completo,
+                tipo_documento,
+                numero_documento,
+                fecha_vto,
+            )
+            if not es_valido:
+                return mensaje  # Devuelve el mensaje de error de validación
 
-        pago_exitoso, mensaje = procesar_pago("tarjeta", cotizacion.importe)
-        if pago_exitoso:
+            pago_exitoso, mensaje = procesar_pago("tarjeta", cotizacion.importe)
+            if pago_exitoso:
+                cotizacion.estado = "Confirmado"
+                notificacion_push = enviar_notificacion_push(
+                    cotizacion.transportista.nombre, forma_pago
+                )
+                mail = enviar_email_confirmacion(
+                    cotizacion.transportista.nombre, 
+                    transportista_email,  
+                    forma_pago
+                )
+                return f"Pago procesado correctamente. Número de pago {mensaje}.\n\n{notificacion_push}\n\n{mail}"
+            else:
+                return f"Pago rechazado: {mensaje}."  # Devuelve el mensaje de saldo insuficiente
+        else:
+            return f"Ya ha sido aceptado"
+    else:
+        if cotizacion.estado != "Confirmado":
             cotizacion.estado = "Confirmado"
+
+            # Enviar notificación push
             notificacion_push = enviar_notificacion_push(
                 cotizacion.transportista.nombre, forma_pago
             )
+
+            # Enviar email al transportista
             mail = enviar_email_confirmacion(
-                cotizacion.transportista.nombre, 
+                cotizacion.transportista.nombre,
                 transportista_email,  
                 forma_pago
             )
-            return f"Pago procesado correctamente. Número de pago {mensaje}.\n\n{notificacion_push}\n\n{mail}"
+            return f"{notificacion_push}\n\n{mail}"
         else:
-            return f"Pago rechazado: {mensaje}."  # Devuelve el mensaje de saldo insuficiente
-    else:
-        cotizacion.estado = "Confirmado"
-
-        # Enviar notificación push
-        notificacion_push = enviar_notificacion_push(
-            cotizacion.transportista.nombre, forma_pago
-        )
-
-        # Enviar email al transportista
-        mail = enviar_email_confirmacion(
-            cotizacion.transportista.nombre,
-            transportista_email,  
-            forma_pago
-        )
-        return f"{notificacion_push}\n\n{mail}"
+            return f"Ya ha sido aceptado"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- TrabajosPracticos/TrabajosPracticosEvaluables/TP_6/TP 6/utils/pago.py
+++ TrabajosPracticos/TrabajosPracticosEvaluables/TP_6/TP 6/utils/pago.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+import random
 
 def validar_tarjeta(numero_tarjeta, pin, nombre_completo, tipo_documento, numero_documento, fecha_vencimiento):
     # Validar cada campo
@@ -12,7 +13,6 @@ def validar_tarjeta(numero_tarjeta, pin, nombre_completo, tipo_documento, numero
     if not all([pin_valid, tarjeta_valid, nombre_valid, documento_valid, vencimiento_valid]):
         return False, "Error en datos de la tarjeta."
     
-    
     # Validar que la fecha de vencimiento es futura
     try:
         mes, anio = map(int, fecha_vencimiento.split('/'))
@@ -20,11 +20,9 @@ def validar_tarjeta(numero_tarjeta, pin, nombre_completo, tipo_documento, numero
             return False, "Mes inválido"
         fecha_vto = datetime(year=anio + 2000, month=mes, day=1)
         if fecha_vto < datetime.now():
-            return False, "por el try Tarjeta vencida."
+            return False, "Tarjeta vencida."
     except ValueError:
-        return False, "por el expet Tarjeta vencida."
-
-
+        return False, "Tarjeta vencida."
     return True, "Datos correctos de la tarjeta."
 
 def procesar_pago(metodo_pago, importe):
@@ -34,5 +32,8 @@ def procesar_pago(metodo_pago, importe):
     if metodo_pago == "tarjeta" and importe > saldo_disponible:
         return False, "Saldo insuficiente"  # Pago rechazado por saldo insuficiente
     
+     # Si el pago es exitoso, generamos un número de transacción aleatorio
+    numero_transaccion = str(random.randint(100000000, 999999999))  # Número aleatorio de 9 dígitos
+
     # Si el pago es exitoso, devolvemos el número de transacción
-    return True, "123456789"  
+    return True, numero_transaccion  
